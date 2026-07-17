ced3a913e01d5f9be28e8dcc43f2c89a417c1592
Emmanuel Great <greatema03@gmail.com>
2026-07-17 13:40:55 +0100

    updated product card

--- src/Product-Component/Component/ProductCard.jsx
+++ src/Product-Component/Component/ProductCard.jsx
@@ -1,7 +1,14 @@
 function ProductCard({ item, onAdd }) {
   return (
     <div className="perfume-card flex flex-col items-center">
-      <img src={item.img} alt={item.name} className="w-[150px] h-[150px]" />
+      {/* Standardized Wrapper Card */}
+    <div className="w-full aspect-square bg-white flex items-center justify-center p-4 rounded-sm">
+      <img 
+        src={item.img} 
+        alt={item.name} 
+        className="max-h-full max-w-full object-contain" 
+      />
+    </div>
       <h3 className="text-lg mb-[10px]">{item.name}</h3>
       <p className="text-md font-[700] mb-[10px]">₦{item.price}</p>
       <button onClick={() => onAdd(item)} className="bg-primary py-[5px] px-[15px] 
